c418788d2537b88cd1b57697168817f82a65c855
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-05-24 22:58:39 +0300

    mon 16.6. fifth exercise

---
+++ 1_5_Lunch/sources/main.swift
@@ -0,0 +1,9 @@
+let lunch = "pizza"
+
+if (lunch == "pizza") {
+    print("Woah! Amazing!")
+} else if (lunch == "burger") {
+    print("So tasty! Yummy!")
+} else {
+    print("I'm not feeling hungry anymore")
+}
